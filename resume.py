d2d15ff1a7c3f866a088f6a1bda11c260f2ce499
techeditorsrc <71694483+techeditorsrc@users.noreply.github.com>
2021-07-05 06:48:15 +0300

    Update resume.py

--- resume.py
+++ resume.py
@@ -77,26 +77,52 @@ class resume():
     
     def setstyle(self,paragraph,run,name):
         font = run.font
-        font_size=10
-        font.size=Pt(font_size)
-        font.name='Dejavu Sans'
+        font_size = 10
+        font.size = Pt(font_size)
+        font.name = 'Dejavu Sans'
         paragraph.paragraph_format.space_before = Pt(2)
         paragraph.paragraph_format.space_after = Pt(2)
         if name=='name':
-            run.bold=True
-            font.size=Pt(font_size)
+            run.bold = True
+            font.size = Pt(font_size)
         if name=='speciality':
-            font.size=Pt(font_size)
+            font.size = Pt(font_size)
         if name=='contacts':
-            font.size=Pt(font_size-4)
+            font.size  =Pt(font_size-2)
         if name=='section_name':
-            font.size=Pt(font_size)
+            font.size = Pt(font_size)
             font.color.rgb = RGBColor(65, 138, 183)
         if name=='paragraph_name':
-            font.size=Pt(font_size)
+            font.size = Pt(font_size)
             font.color.rgb = RGBColor(53, 108, 132)
         if name=='paragraph_text':
-            font.size=Pt(font_size)
+            font.size = Pt(font_size)
+
+    def w(self,n,**p):
+        xml=docx.oxml.shared.OxmlElement('w:'+n)
+        for key,value in p.items():
+            xml.set(docx.oxml.shared.qn('w:'+key), value)
+        return xml
+
+    def add_hyperlink(self,paragraph,url,text,color,font_size,font_name):
+        part = paragraph.part
+        r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+        hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+        hyperlink.set(docx.oxml.shared.qn('r:id'), r_id)
+        new_run = docx.oxml.shared.OxmlElement('w:r')
+        rPr = docx.oxml.shared.OxmlElement('w:rPr')
+        if not color is None:
+            rPr.append(self.w('color',val=color))
+        rPr.append(self.w('u',val='none'))
+        f = self.w('rFonts',ascii=font_name,hAnsi=font_name,cs=font_name,eastAsia=font_name)
+        rPr.append(f)
+        f = self.w('sz',val=str(font_size*2))
+        rPr.append(f)
+        new_run.append(rPr)
+        new_run.text = text
+        hyperlink.append(new_run)
+        paragraph._p.append(hyperlink)
+        return hyperlink
 
     #render name and speciality
     def render_name(self):
@@ -110,9 +136,10 @@ class resume():
     #render contscts
     def render_contact(self,contact_name,contact_address):
         paragraph = self.document.add_paragraph()
-        run=paragraph.add_run(contact_name+': '+contact_address)
+        run=paragraph.add_run(contact_name+': ')
         self.setstyle(paragraph,run,'contacts')
-        
+        self.add_hyperlink(paragraph,contact_address,contact_address.replace("mailto:",""),'729fcf',8,'Dejavu Sans')        
+    
     def render_contacts(self):
         for contact in self.contacts:
             self.render_contact(contact['contact_name'],contact['contact_address'])
